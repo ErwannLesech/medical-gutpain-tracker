eb3a58679db7f36f0832f77751b92b92ae8058f4
R-One <erwann.lesech@epita.fr>
2026-01-28 16:30:00 +0100

    Update build configurations and dependencies
    - Update Android Gradle configuration
    - Refresh platform-specific generated files
    - Update project dependencies in pubspec
    - Add commit helper script for development

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,8 +7,9 @@ plugins {
 
 android {
     namespace = "com.wakygut.waky_gut"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 35
     ndkVersion = flutter.ndkVersion
+    buildToolsVersion = "35.0.0"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
@@ -26,7 +27,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
