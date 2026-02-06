53c1603c47e72aefdf7858221cf00aac321157f2
R-One <erwann.lesech@epita.fr>
2026-02-06 22:45:37 +0100

    chore: update compileSdk and targetSdk to 36

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.wakygut.waky_gut"
-    compileSdk = 35
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
     buildToolsVersion = "35.0.0"
 
@@ -27,7 +27,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = 35
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
